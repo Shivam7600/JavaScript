b9a0fd74cef911ca3ac425d59dd4c77b3d7ed4af
Shivam7600 <98753099+Shivam7600@users.noreply.github.com>
2023-11-30 18:59:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+ "use treat"; // treat all JS code as newer version.
+
+ // alert(3 + 3) // we are using node js, not browser.
+
+ console.log(3 + 3)
+// code readability should be high. 
+
+console.log("shivam")
+
+// JavaScript has 8 data types. 
+let name = "shivam"         // 1. string
+let age = 24                // 2. number
+                            // 3. bigint
+let isLoggedIn = false;     // 4. boolean (true)
+let state;                  // 5. null
+                            // 6. undefined
+                            // 7. symbol
+                            
+                            // 8. object
+ 
+/* an object data type can contain 1. An Object
+                                   2. An Array
+                                   3. A Date
+*/
+
+//object
+const person = {
+    firstName: "Shivam",
+    LastName: "Singh"
+}
+
+//Array object
+const cars = ["mercedes", "BMW", "ferrari"]
+
+//De Object
+const dates = new dates("2022-03-25")
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);      //object
